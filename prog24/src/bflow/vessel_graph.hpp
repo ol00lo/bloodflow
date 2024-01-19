dcdc505eb67befb38a2247380c0bde0baf0b9120
ol00lo <115536900+ol00lo@users.noreply.github.com>
2024-01-19 09:39:57 +0300

    (LIZA-2) Создать класс, описывющий область расчёта. (#2)
    Создать класс, описывющий область расчёта.

---
+++ prog24/src/bflow/vessel_graph.hpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <array>
+
+namespace bflow {
+	class VesselGraph {
+	public:
+		VesselGraph(const std::vector<std::vector<int>>& node, const std::vector<double>& len);
+		std::array<int, 2> tab_edge_node(int iedge) const;
+		std::vector<int> tab_node_edge(int inode) const;
+		int find_edge_by_nodes(int inode, int jnode) const;
+		double find_length(int iedge) const;
+		int n_nodes() const;
+		int n_edges() const;
+	private:
+		std::vector<std::vector<int>> _nodes_edges;
+		std::vector<double> _edge_lengths;
+		int _n_edges;
+	};
+}
--- prog24/src/tests/graph_test.cpp
+++ prog24/src/tests/graph_test.cpp
@@ -1,5 +1,25 @@
 #include "catch.hpp"
+#include <vector>
+#include <map>
+#include "bflow/vessel_graph.hpp"
+
+using namespace bflow;
 
 TEST_CASE("basic graph functionality", "[graph]") {
-	CHECK(1 == 1);
+	std::vector<std::vector<int>> node = { {0,1,2}, {0}, {1, 3, 4, 5}, {2, 6, 7}, {3}, {4}, {5}, {6, 8, 9, 10}, {7}, {8}, {9}, {10} };
+	std::vector<double> ed = { 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0 };
+	VesselGraph gr1(node, ed);
+	CHECK(gr1.n_edges() == ed.size());
+	CHECK(gr1.find_length(7) == 8.0);
+	CHECK(gr1.tab_edge_node(4)[0] == 2);
+	CHECK(gr1.tab_edge_node(4)[1] == 5);
+}
+
+TEST_CASE("improper graph definition", "[improper_graph]") {
+	CHECK_THROWS(VesselGraph({ {0,1,2} }, { 1.0, 2.0 }));
+	CHECK_THROWS(VesselGraph({ {0,1,2}, {0}, {1}, {2} }, { 1.0, 2.0 }));
+	CHECK_THROWS(VesselGraph({ {0,1,2}, {0}, {1}, {1} }, { 1.0, 2.0, 3.0 }));
+	CHECK_THROWS(VesselGraph({ {0,1,2}, {0}, {1} }, { 1.0, 2.0 }));
+	CHECK_THROWS(VesselGraph({ {0,1,2,2}, {0}, {1} }, { 1.0, 2.0 }));
+	CHECK_THROWS(VesselGraph({ {0,1,2,2}, {0}, {1} }, { 1.0, 2.0, 3.0 }));
 }
